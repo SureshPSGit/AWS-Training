99870ff7978338eaddf3ee9f2ab1adeb46e44743
sureshlikesyou@gmail.com <Survani@16>
2020-01-14 21:24:29 +0100

    Initial check-in of module AWS-Training

---
+++ AWS-Training/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AWS_Training.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        public HelloWorldController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World application is up and running";
+        }
+    }
+}
